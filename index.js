0578769e223f39bda9367e57ab08f113b622d1ec
Joe Hitchens <joe@sleepless.com>
2017-04-29 22:18:54 -0700

    -

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
+//var o2j = function(o) { try { return JSON.stringify(o) } catch(e) { return null } }
+//var D = function(o) { console.log(o2j(o)) };
+
 var TimeHash = function(in_opts) {
 
 	var opts = {
-		timeout_seconds: 10,
+		scan_interval: 10 * 1000,
 	}
 	for(var k in in_opts) { if(in_opts[k]) opts[k] = in_opts[k] }
+
+	var mstime = function() { return new Date().getTime() }
 
 
 	var self = this;
@@ -13,7 +18,7 @@ var TimeHash = function(in_opts) {
 	var num = 0			// # msgs waiting
 
 	// Remove and return a msg from the list given its id
-	var rem = self.rem = function(id) {
+	var remove = self.remove = function(id) {
 		var p = null
 		var w = wraps[id]
 		if(w) {
@@ -21,11 +26,9 @@ var TimeHash = function(in_opts) {
 			delete wraps[id]
 			num -= 1
 			if(num == 0) {
-				// D("clearInterval "+timer)
 				clearInterval(timer)
 				timer = null
 			}
-			// D("forgetting "+id+" "+o2j(p));
 		}
 		return p
 	}
@@ -33,33 +36,60 @@ var TimeHash = function(in_opts) {
 
 	// Put a msg into the list
 	// ttl is in secs and should not be less than 10 (default is 60 if not provided)
-	var ins = self.ins = function(p, id, ttl) {
-		//D("remembering "+id+" "+o2j(p));
+	var insert = self.insert = function(p, id, ttl) {
+		var old = wraps[id];
 		var w = {
-			expire: time() + (ttl || 60),
+			expire: mstime() + (ttl || (60 * 1000)),
 			payload: p,
 		}
 		wraps[id] = w;
-		num += 1
+		if(old === undefined) {
+			num += 1
+		}
 		if(num == 1) {
 			timer = setInterval(function() {
-				var t = time()
+				var t = mstime()
 				for(var k in wraps) {
 					var w = wraps[k];
 					if(t >= w.expire) {
-						rem(k);
+						// it's expired ... toss it.
+						remove(k);
 					}
 				}
-			}, opts.timeout_seconds * 1000);
-			//D("setInterval "+timer)
+			}, opts.scan_interval);
 		}
+		return old;
 	}
 
 }
 
+
 if((typeof process) !== 'undefined') {
 	// node.js
+
 	module.exports = TimeHash
+
+	if(require && require.main === module) {
+		// module being executed directly; run tests
+		var throwIf = function(c, s) { if(c) { throw new Error(s || "FAILED"); } }
+		var L = function(o) { console.log(o) };
+
+		L("Running tests");
+
+		var th = new TimeHash({scan_interval: 1000})
+
+		th.insert("payload", "key", 4000)
+		throwIf(th.remove("key") !== "payload", "test 1 failed")
+		L("pass 1");
+
+		th.insert("payload", "key", 4000)
+		setTimeout(function() {
+			throwIf(th.remove("key") !== null, "test 2 failed")
+			L("pass 2");
+		}, 5000);
+
+		var th = new TimeHash()
+	}
 }
 
 
